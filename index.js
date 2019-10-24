1795726e95d2019eb1016e9485ea407d20b90644
Monica Marziano <mmarziano1@gmail.com>
2019-10-24 15:53:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -64,6 +64,7 @@ function findEmployeeByFirstName(emps, name) {
 }
 
 
+
 /*
  We're giving you this function. Take a look at it, you might see some usage
  that's new and different. That's because we're avoiding a well-known, but
